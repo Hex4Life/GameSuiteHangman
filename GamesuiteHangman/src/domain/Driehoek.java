28aeb97535b61cd6125fefcb9e6eb9c07aa74a1a
Sofie <sofie.vandevonder@student.ucll.be>
2017-05-09 14:09:24 +0200

    Denker: Kevin Vanthuyne Doener: Sofie Van de Vonder US: 06 Class: Driehoek, Cirkel Status: driehoek: bezig, cirkel: af

---
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -0,0 +1,80 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driehoek extends Vorm{
+
+
+	private Punt hoekPunt1;
+	private Punt hoekPunt2;
+	private Punt hoekPunt3;
+	
+	public Driehoek(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3){
+		setHoekPunten(hoekPunt1, hoekPunt2, hoekPunt3);
+	}
+	
+	
+	public void setHoekPunten(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3){
+		if(hoekPunt1 == null || hoekPunt2 == null || hoekPunt3 == null){
+			throw new DomainException("Hoekpunt mag niet null zijn");
+		}
+		if(((hoekPunt2.getX()-hoekPunt1.getX())*(hoekPunt3.getY() -hoekPunt1.getY())) 
+				== ((hoekPunt3.getX() - hoekPunt1.getX())* (hoekPunt2.getY() - hoekPunt1.getY()))){
+			throw new DomainException("Hoekpunten mogen niet op 1 rechte liggen.");
+			
+		}
+		this.hoekPunt1 = hoekPunt1;
+		this.hoekPunt2 = hoekPunt2;
+		this.hoekPunt3 = hoekPunt3;
+	}
+
+
+	public Punt getHoekPunt1() {
+		return hoekPunt1;
+	}
+
+
+	public Punt getHoekPunt2() {
+		return hoekPunt2;
+	}
+
+
+	public Punt getHoekPunt3() {
+		return hoekPunt3;
+	}
+	
+	
+	
+	
+	public boolean equals(Object o){
+		if(o instanceof Driehoek){
+			Driehoek driehoek = (Driehoek) o;
+			List<Punt> lijsthoekpunten1 = new ArrayList<>();
+			List<Punt> lijsthoekpunten2 = new ArrayList<>();
+			lijsthoekpunten1.add(hoekPunt1);
+			lijsthoekpunten1.add(hoekPunt2);
+			lijsthoekpunten1.add(hoekPunt3);
+			
+			lijsthoekpunten2.add(driehoek.getHoekPunt1());
+			lijsthoekpunten2.add(driehoek.getHoekPunt2());
+			lijsthoekpunten2.add(driehoek.getHoekPunt3());
+			
+			
+			if(lijsthoekpunten1.containsAll(lijsthoekpunten2)&&lijsthoekpunten2.contains(lijsthoekpunten1)){
+				return true;
+			}
+			
+			
+		}
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
