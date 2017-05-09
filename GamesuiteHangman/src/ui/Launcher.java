a6c2e0b127aef3221290f9b78249a7276cbe4183
MattiVM <matthias67r@gmail.com>
2017-05-09 11:59:49 +0200

    Denker: Matthias Van Meerbeek Doener: Thibault Van Acker User story: 02 Klasse: Launcher.java Status: Done

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,6 +2,7 @@ package ui;
 
 import javax.swing.JOptionPane;
 
+import domain.Punt;
 import domain.Speler;
 
 public class Launcher {
@@ -11,6 +12,13 @@ public class Launcher {
 		Speler speler = new Speler(naam);
 		JOptionPane.showMessageDialog(null, "... heeft als score: " + speler.getScore());
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		
+		int xcoor;
+		xcoor = Integer.parseInt(JOptionPane.showInputDialog("x coordinaat van het punt: "));
+		int ycoor;
+		ycoor = Integer.parseInt(JOptionPane.showInputDialog("y coordinaat van het punt: "));
+		Punt punt1 = new Punt(xcoor, ycoor);
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt1);
 	}
 	
 
