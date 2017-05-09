45b719722279e715211840a069b97ac4e5800ea1
Wouter Engelen <wouter.engelen@student.ucll.be>
2017-05-09 13:15:33 +0200

    DomainException refactor

--- GamesuiteHangman/src/domain/DomainException.java
+++ GamesuiteHangman/src/domain/DomainException.java
@@ -1,9 +1,13 @@
 package domain;
 
 public class DomainException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
 	public DomainException (){
 		super();
 	}
+	
 	public DomainException(String message){
 		super(message);
 	}
