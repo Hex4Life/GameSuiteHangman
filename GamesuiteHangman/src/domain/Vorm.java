b697edb34d1a65e9afeb13d34cb15262e4c61045
Sofie <sofie.vandevonder@student.ucll.be>
2017-05-11 13:11:17 +0200

    Doener: Sofie Van de Vonder Story: 14 Class: TekenHangMan, TekenHangManTest Status: gestart

--- GamesuiteHangman/src/domain/Vorm.java
+++ GamesuiteHangman/src/domain/Vorm.java
@@ -1,9 +1,19 @@
 package domain;
 
-public abstract class Vorm implements Drawable{
-	
+public abstract class Vorm implements Drawable {
+
+	public boolean zichtbaar;
+
 	public abstract boolean equals(Object o);
-	
-	
+
 	public abstract Omhullende getOmhullende();
+	
+	public boolean isZichtbaar(){
+		return zichtbaar;
+	}
+	
+	public void setZichtbaar(boolean zichtbaar){
+		this.zichtbaar = zichtbaar;
+	}
+	
 }
