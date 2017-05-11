7f3d4b5f94da44c6d7134d604a6079dc741a41d2
thibaultvanacker <thibaultvanacker@bitbucket.org>
2017-05-11 15:01:46 +0200

    Doener: Thibault Van Acker Denker: Sofie Van de Vonder Story: 14 Class: TekenHangMan, TekenHangManTest Status: af

--- GamesuiteHangman/src/domain/TekenHangMan.java
+++ GamesuiteHangman/src/domain/TekenHangMan.java
@@ -1,55 +1,75 @@
 package domain;
 
-public class TekenHangMan extends Tekening{
+public class TekenHangMan extends Tekening {
+		
+	private int huidigeZichtbaar = 4;
 	
-	
-	
-	private String naam;
+	public TekenHangMan() {
+		super("HangMan");
+		Vorm galgBodem = new Rechthoek(new Punt(10, 350), 300, 40);
+		Vorm galgStaaf = new LijnStuk(new Punt(160, 350), new Punt(160, 50));
+		Vorm hangbar = new LijnStuk(new Punt(160, 50), new Punt(280, 50));
+		Vorm koord = new LijnStuk(new Punt(280, 50), new Punt(280, 100));
+		Vorm hoofd = new Cirkel(new Punt(280, 125), 25);
+		Vorm oogLinks = new Cirkel(new Punt(270, 118), 2);
+		Vorm oogRechts = new Cirkel(new Punt(290, 118), 2);
+		Vorm neus = new Cirkel(new Punt(280, 128), 2);
+		Vorm mond = new LijnStuk(new Punt(270, 138), new Punt(290, 138));
+		Vorm lijf = new LijnStuk(new Punt(280, 150), new Punt(280, 250));
+		Vorm beenLinks = new LijnStuk(new Punt(280, 250), new Punt(240, 310));
+		Vorm beenRechts = new LijnStuk(new Punt(280, 250), new Punt(320, 310));
+		Vorm voetLinks = new Cirkel(new Punt(240, 310), 5);
+		Vorm voetRechts = new Cirkel(new Punt(320, 310), 5);
+		Vorm armLinks = new LijnStuk(new Punt(280, 200), new Punt(230, 170));
+		Vorm armRechts = new LijnStuk(new Punt(280, 200), new Punt(330, 170));
+		Vorm handLinks = new Cirkel(new Punt(230, 170), 5);
+		Vorm handRechts = new Cirkel(new Punt(330, 170), 5);
+		super.voegToe(galgBodem);
+		super.voegToe(galgStaaf);
+		super.voegToe(hangbar);
+		super.voegToe(koord);
+		super.voegToe(hoofd);
+		super.voegToe(oogLinks);
+		super.voegToe(oogRechts);
+		super.voegToe(neus);
+		super.voegToe(mond);
+		super.voegToe(lijf);
+		super.voegToe(beenLinks);
+		super.voegToe(beenRechts);
+		super.voegToe(voetLinks);
+		super.voegToe(voetRechts);
+		super.voegToe(armLinks);
+		super.voegToe(armRechts);
+		super.voegToe(handLinks);
+		super.voegToe(handRechts);
+	}
 
-	public TekenHangMan(String naam) {
-		super(naam);
-		// TODO Auto-generated constructor stub
+	public int getAantalOnzichtbare() {
+		return getAantalVormen()-huidigeZichtbaar;
+	}
+
+	public void zetVolgendeZichtbaar() {
+		if(huidigeZichtbaar == getAantalVormen()){
+			throw new DomainException("Geen onzichtbare vormen meer");
+		}
+		super.getVorm(huidigeZichtbaar).setZichtbaar(true);
+		huidigeZichtbaar++;
+	}
+
+	public void reset() {
+		for (int i = 4; i < super.getVormen().size(); i++) {
+			super.getVormen().get(i).setZichtbaar(false);
+		}
+		huidigeZichtbaar = 4;
 	}
 	
-	
-	
-	
-	public String getNaam(){
-		return naam;
+	@Override
+	public void voegToe(Vorm vorm){
+		throw new DomainException("Kan geen extra vormen toevoegen aan TekeningHangMan");
 	}
-	public int getAantalOnzichtbaar(){
-		
+	@Override
+	public void verwijder(Vorm vorm){
+		throw new DomainException("Kan geen vormen verwijderen uit TekeningHangMan");
 	}
-	public void zetVolgendeZichtbaar(){
-		
-	}
-	
-	public void reset(){
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- GamesuiteHangman/test/domain/TekenHangManTest.java
+++ GamesuiteHangman/test/domain/TekenHangManTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,32 +14,45 @@ public class TekenHangManTest {
 
 	@Test
 	public void Afbeelding_Hangman_maakt_een_hangman_afbeelding_met_de_naam_hangman_en_18_vormen_waarvan_14_onzichtbaar() {
-
+		TekenHangMan tekening1 = new TekenHangMan();
+		assertEquals(14, tekening1.getAantalOnzichtbare());
 	}
 
 	@Test
 	public void zetVolgendeZichtbaar_moet_eerste_onzichtbare_vorm_zichtbaar_zetten() {
-
+		TekenHangMan tekening1 = new TekenHangMan();
+		assertEquals(14, tekening1.getAantalOnzichtbare());
+		tekening1.zetVolgendeZichtbaar();
+		assertEquals(13, tekening1.getAantalOnzichtbare());
 	}
 
-	@Test
+	@Test(expected=DomainException.class)
 	public void zetVolgendeZichtbaar_moet_exception_gooien_als_alle_vormen_zichtbaar() {
-
+		TekenHangMan tekening1 = new TekenHangMan();
+		assertEquals(14, tekening1.getAantalOnzichtbare());
+		for(int i=0; i<15; i++){
+			tekening1.zetVolgendeZichtbaar();
+		}
 	}
 
 	@Test
 	public void reset_moet_alle_vormen_behalve_galg_weer_onzichtbaar_zetten() {
-
+		TekenHangMan tekening1 = new TekenHangMan();
+		assertEquals(14, tekening1.getAantalOnzichtbare());
+		tekening1.zetVolgendeZichtbaar();
+		assertEquals(13, tekening1.getAantalOnzichtbare());
+		tekening1.reset();
+		assertEquals(14, tekening1.getAantalOnzichtbare());
 	}
 
-	@Test
+	@Test(expected=DomainException.class)
 	public void voegToe_moet_een_exception_gooien() {
-
+		new TekenHangMan().voegToe(new Rechthoek(new Punt(5, 10), 2, 8));
 	}
 
-	@Test
+	@Test(expected=DomainException.class)
 	public void verwijder_moet_een_exception_gooien() {
-
+		new TekenHangMan().verwijder(new Rechthoek(new Punt(5, 10), 2, 8));
 	}
 
 }
