e936c7eeb182466ea1bd7f0c2d8607cbce96efed
MattiVM <matthias67r@gmail.com>
2017-05-12 10:33:19 +0200

    Bewerkt door: Matthias Van Meerbeek Klasse: AllTest.java Status: Update

--- GamesuiteHangman/test/domain/AllTests.java
+++ GamesuiteHangman/test/domain/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, RechthoekTest.class, LijnStukTest.class, DriehoekTest.class, TekeningTest.class,
-				HangManTest.class, HintLetter.class, HintWoord.class, Omhullende.class, WoordenLijstTest.class})
+				HangManTest.class, HintLetterTest.class, HintWoordTest.class, OmhullendeTest.class, WoordenLijstTest.class, TekenHangManTest.class})
 public class AllTests {
 
 }
